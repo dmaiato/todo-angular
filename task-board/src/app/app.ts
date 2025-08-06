be0e80b865cf9376168383b2414397712a5cc497
dmaiato <dmaiato.dev@gmail.com>
2025-08-06 10:08:34 -0300

    feat: added task-card component

--- task-board/src/app/app.ts
+++ task-board/src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('task-board');
+  protected readonly header = signal('HEADER');
 }
