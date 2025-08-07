83e19cc0c140de273a8d7528db65eade1f6c5ffa
dmaiato <dmaiato.dev@gmail.com>
2025-08-07 09:45:00 -0300

    feat: completed initial api service code

--- task-board/src/app/services/task-service.ts
+++ task-board/src/app/services/task-service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Task } from '../models/task.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  
+  private apiUrl = 'http://localhost:5280';
+  private tasksSubject = new BehaviorSubject<Task[]>([]);
+  public tasks$ = this.tasksSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    this.loadTasks();
+  }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/tasks`)
+      .pipe(tap((tasks: Task[]) => this.tasksSubject.next(tasks)));
+  }
+
+  createTask(task: Omit<Task, 'id'>): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/tasks`, task)
+      .pipe(tap(() => this.loadTasks()));
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/tasks/${task.id}`, task)
+      .pipe(tap(() => this.loadTasks()));
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/tasks/${id}`)
+      .pipe(tap(() => this.loadTasks()));
+  }
+
+  private loadTasks(): void {
+    this.getTasks().subscribe();
+  }
 }
